d7f57c9b325b104e5bcdffe4bae5e62dd4211fbd
mkh9293 <mkh9293@gmail.com>
2024-06-08 16:11:16 +0900

    first commit

---
+++ src/main/java/com/example/circuit_breaker_playground/CircuitBreakerPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.circuit_breaker_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitBreakerPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitBreakerPlaygroundApplication.class, args);
+	}
+
+}
